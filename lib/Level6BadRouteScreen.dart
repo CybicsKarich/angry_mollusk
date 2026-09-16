fba84a1609d132fa86e219d6fdbe9ebb69356465
CybicsKarich <novikovvasya1974@gmail.com>
2026-09-16 13:16:23 +0300

    Create Level6BadRouteScreen.dart

---
+++ lib/Level6BadRouteScreen.dart
@@ -0,0 +1,26 @@
+class Level6BadRouteScreen extends StatelessWidget {
+  const Level6BadRouteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "ПЛОХАЯ ЛИНИЯ (В РАЗРАБОТКЕ)",
+              style: TextStyle(color: Colors.red, fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Назад"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
